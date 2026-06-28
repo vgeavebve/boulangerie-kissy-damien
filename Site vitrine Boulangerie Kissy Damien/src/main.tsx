8090a9693ad70bb376dda58ad3563cb998450f3e
vgeavebve <eliasbuchholz38@gmail.com>
2026-06-28 21:04:14 +0200

    Remove duplicate import statements in main.tsx

--- Site vitrine Boulangerie Kissy Damien/src/main.tsx
+++ Site vitrine Boulangerie Kissy Damien/src/main.tsx
@@ -1,7 +1,8 @@
+import { createRoot } from "react-dom/client";
+import App from "./app/App.tsx";
+import "./styles/index.css";
+import { inject } from "@vercel/analytics";
 
-  import { createRoot } from "react-dom/client";
-  import App from "./app/App.tsx";
-  import "./styles/index.css";
+inject();
 
-  createRoot(document.getElementById("root")!).render(<App />);
-  
+createRoot(document.getElementById("root")!).render(<App />);
